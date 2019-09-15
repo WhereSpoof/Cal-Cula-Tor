5e947e3179851183f3c52feb977ab9a1c8553d28
nikita-efimov <nikikot@list.ru>
2019-09-15 15:26:38 +0300

    auto currency upd

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
-while true
-do
+# while true
+# do
     python3.7 main.py > js/curr.js
     git add .
     git commit -m 'auto currency upd'
     git push
-    sleep 7200
-done
+    # sleep 7200
+# done
